decfbd0229c33077177809833f18ccd1d535346a
conrad <z5478184@ad.unsw.edu.au>
2026-06-08 19:07:21 +1000

    wk1

---
+++ 01/starter_code/face.c
@@ -0,0 +1,42 @@
+// face.c
+//
+// Written by Conrad Vernon (z5478184),
+// on 2 June 2026 
+//
+// The program prints two simple faces
+//
+// Face 1:
+//   ~ ~
+//  0 0
+//   o
+//   -
+
+#include <stdio.h>
+
+int main(void) {
+
+    // printf("~ ~\n0 0\n o \n - \n");
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf(" - \n");
+
+    printf("\n\n");
+
+    printf("~ ~\n");
+    printf("0 0\n");
+    printf(" o \n");
+    printf("\\\\_//\n");
+
+    return 0;
+}
+
+
+//
+// Face 2:
+//  ~ ~
+//  0 0
+//   o
+//  \_/
+
+
---
+++ 01/starter_code/hello_world.c
@@ -0,0 +1,20 @@
+// Basic Hello, World! program
+
+#include <stdio.h>
+
+int main(void) {
+    // This prints "Hello, World!" to the terminal, and the next terminal
+    // command is printed on the line below it, instead of on the same
+    // line.
+    // What does this imply about what '\n' does?
+    /*a
+    a
+    a
+    a
+    a
+    aa
+     */
+    printf("Hello, World!");
+    
+    return 0;
+}
